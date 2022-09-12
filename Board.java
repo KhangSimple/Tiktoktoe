e63ea1bd1b06466e7d9bc5de84c3c5fd91929080
KhangSimple <you@example.com>
2022-09-12 08:57:11 +0700

    Test commmit

--- Board.java
+++ Board.java
@@ -1,11 +1,14 @@
 public class Board {
-    public static void printBoard(CellStatic cell[][]){
+    public static void printBoard(CellStatic cell[][]) {
         System.out.print("\n");
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                if(cell[i][j]==CellStatic.Empty) System.out.print("[ ] ");
-                if(cell[i][j]==CellStatic.X) System.out.print("[X] ");
-                if(cell[i][j]==CellStatic.O) System.out.print("[O] ");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (cell[i][j] == CellStatic.Empty)
+                    System.out.print("[ ] ");
+                if (cell[i][j] == CellStatic.X)
+                    System.out.print("[X] ");
+                if (cell[i][j] == CellStatic.O)
+                    System.out.print("[O] ");
             }
             System.out.print("\n");
         }
